48a7b676f2aeaa3a23200e6ff6fa14a63378e71f
Vivek Rallapally <vivek.rallapally@pxpfinancial.com>
2025-02-13 12:11:09 +0530

    results table

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ function LabTabs() {
         </Box>
         <TabPanel value="1">
           <BasicTable />
+          <div></div>
           <NormalTable />
         </TabPanel>
         <TabPanel value="2">Item Two</TabPanel>
@@ -98,16 +99,14 @@ function BasicTable() {
 }
 
 // Results table
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(no, pay_id, pred_status, true_status, conf_sc) {
+  return { no, pay_id, pred_status, true_status, conf_sc };
 }
 
 const rows1 = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData(1, 9493786284, "Fraudulent", "Fraudulent", 0.78483),
+  createData(2, 8628494937, "Legitimate", "Fraudulent", 0.56714),
+  createData(3, 8628494937, "Legitimate", "Legitimate", 0.56714),
 ];
 
 function NormalTable() {
@@ -116,11 +115,11 @@ function NormalTable() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>S.No</TableCell>
+            <TableCell align="right">Payment ID</TableCell>
+            <TableCell align="right">Predicted Status</TableCell>
+            <TableCell align="right">True Status</TableCell>
+            <TableCell align="right">Confidence Score</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -130,12 +129,12 @@ function NormalTable() {
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.no}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.pay_id}</TableCell>
+              <TableCell align="right">{row.pred_status}</TableCell>
+              <TableCell align="right">{row.true_status}</TableCell>
+              <TableCell align="right">{row.conf_sc}</TableCell>
             </TableRow>
           ))}
         </TableBody>
